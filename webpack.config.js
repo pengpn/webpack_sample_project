5032a7287251dfb7774cb89d1e901207fc6ae111
pengpn <312924860@qq.com>
2017-07-31 17:58:33 +0800

    modify

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,14 @@ module.exports = {
             'style-loader',
             'css-loader'
         ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',//在webpack的module部分的loaders里进行配置即可
+        query: {
+          presets: ['es2015','react']
+        }
       }
     ]
   },
